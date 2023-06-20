0cd2f8377ca75d2e0b6328e18653dd99de0a3acf
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-06-19 21:48:10 -0300

    FEAT: animation

--- src/app/modules/home/hero/hero.component.ts
+++ src/app/modules/home/hero/hero.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { bounceInUpAnimation, bounceInUpOnEnterAnimation } from 'angular-animations';
+import { Component, OnInit } from '@angular/core';
+import { bounceInUpAnimation, bounceInUpOnEnterAnimation, fadeInOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-hero',
@@ -8,9 +8,27 @@ import { bounceInUpAnimation, bounceInUpOnEnterAnimation } from 'angular-animati
   animations: [
     bounceInUpOnEnterAnimation({ anchor: 'enter1' }),
     bounceInUpOnEnterAnimation({ anchor: 'enter2', delay: 200 }),
-    bounceInUpOnEnterAnimation({ anchor: 'enter3', delay: 400 })
+    bounceInUpOnEnterAnimation({ anchor: 'enter3', delay: 400 }),
+    fadeInOnEnterAnimation()
   ]
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
+
+  imageUrl: { image: string, title: string }[] = [
+    { image: 'assets/image/imagem-1.jpeg', title: 'Perfil' },
+    { image: 'assets/image/LOGO-01.png', title: 'Logo Job 1' },
+  ];
+
+  isLoading = true;
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  onImageLoad(event: Event) {
+    this.isLoading = false;
+  }
 
 }

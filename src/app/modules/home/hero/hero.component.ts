fb30d3d3f02cf6856c8ef3b863d9d7353b2f6679
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-06-19 21:14:50 -0300

    FEAT: animation

--- src/app/modules/home/hero/hero.component.ts
+++ src/app/modules/home/hero/hero.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { bounceInUpAnimation, bounceInUpOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
+  animations: [
+    bounceInUpOnEnterAnimation({ anchor: 'enter1' }),
+    bounceInUpOnEnterAnimation({ anchor: 'enter2', delay: 200 }),
+    bounceInUpOnEnterAnimation({ anchor: 'enter3', delay: 400 })
+  ]
 })
 export class HeroComponent {
 

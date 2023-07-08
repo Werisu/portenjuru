5de45d9a1803ea57bbda58d1ac6d1a07f3d9d842
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-07-08 17:03:33 -0300

    fix

--- src/app/modules/home/hero/hero.component.ts
+++ src/app/modules/home/hero/hero.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { bounceInUpOnEnterAnimation, fadeInOnEnterAnimation } from 'angular-animations';
+import { bounceInUpOnEnterAnimation, fadeInOnEnterAnimation, flipInYOnEnterAnimation, flipOutYOnLeaveAnimation, shakeOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-hero',
@@ -9,14 +9,21 @@ import { bounceInUpOnEnterAnimation, fadeInOnEnterAnimation } from 'angular-anim
     bounceInUpOnEnterAnimation({ anchor: 'enter1' }),
     bounceInUpOnEnterAnimation({ anchor: 'enter2', delay: 200 }),
     bounceInUpOnEnterAnimation({ anchor: 'enter3', delay: 400 }),
-    fadeInOnEnterAnimation()
+    fadeInOnEnterAnimation(),
+    flipInYOnEnterAnimation(),
+    flipOutYOnLeaveAnimation(),
+    shakeOnEnterAnimation()
   ]
 })
 export class HeroComponent implements OnInit {
 
+  isFlipped: boolean = false;
+  estadoAcenar = false;
+
   imageUrl: { image: string, title: string }[] = [
-    { image: 'assets/image/imagem-1.jpeg', title: 'Perfil' },
+    { image: 'assets/image/perfil-use.png', title: 'Perfil' },
     { image: 'assets/image/LOGO-01.png', title: 'Logo Job 1' },
+    { image: 'assets/image/imagem-2.webp', title: 'Perfil 2' },
   ];
 
   isLoading = true;
@@ -24,11 +31,22 @@ export class HeroComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-
+    setInterval(() => {
+      this.estadoAcenar = true;
+    }, 1000);
   }
 
   onImageLoad(event: Event) {
     this.isLoading = false;
+  }
+
+  // Método para inverter a imagem
+  flip() {
+    this.isFlipped = !this.isFlipped;
+  }
+
+  acenar(){
+    this.estadoAcenar = !this.estadoAcenar;
   }
 
 }
